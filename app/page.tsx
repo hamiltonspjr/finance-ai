c7ca2a592b1b36e63ac50e7aac4b3d05f2de447a
hamilton <hamilton-souza@outlook.com.br>
2025-02-24 18:41:32 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
+      <Button>Hello world</Button>
     </div>
   );
 }
